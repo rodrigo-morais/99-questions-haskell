dcacc6428664d4c63739a97d291e0b27e5ac5c1b
Rodrigo Morais <morais.rm@gmail.com>
2016-05-29 19:52:13 -0300

    Adding the exercise number 11.

---
+++ 11to20.hs
@@ -0,0 +1,30 @@
+import Data.List (group)
+
+-- exercise 11
+encodeModified :: (Eq a) => [a] -> [(String, Int, a)]
+encodeModified [] = []
+encodeModified (x:xs) = 
+  let
+    qty =
+      length $ x : takeWhile (== x) xs
+
+    modifier =
+      case qty of
+        1 -> "Single"
+        _ -> "Multiple"
+  in
+    [(modifier, qty, x)] ++ encodeModified (dropWhile (==x) xs)
+
+data ListItem a = Single a | Multiple Int a
+  deriving (Show)
+
+encode :: (Eq a) => [a] -> [(Int, a)]
+encode [] = []
+encode (x:xs) = 
+    [(length $ x : takeWhile (== x) xs, x)] ++ encode (dropWhile (==x) xs)
+
+encodeModified' :: (Eq a) => [a] -> [ListItem a]
+encodeModified' = map encodeHelper . encode
+  where
+    encodeHelper (1, x) = Single x
+    encodeHelper (n, x) = Multiple n x
