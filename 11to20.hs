badb83738830d36bee5bb2177b5c4313bf43193a
Rodrigo Morais <morais.rm@gmail.com>
2016-06-03 09:56:26 -0300

    Adding the exercise 13.

--- 11to20.hs
+++ 11to20.hs
@@ -36,3 +36,19 @@ decodeModified = concatMap decodeHelper
   where
     decodeHelper (Single x) = [x]
     decodeHelper (Multiple n x) = replicate n x
+
+
+-- exercise 13
+encode' :: Eq a => [a] -> [(Int,a)]
+encode' = foldr helper []
+    where
+      helper x [] = [(1,x)]
+      helper x (y@(a,b):ys)
+        | x == b    = (1+a,x):ys
+        | otherwise = (1,x):y:ys
+
+encodeDirect :: Eq a => [a] -> [ListItem a]
+encodeDirect = map encodeHelper . encode'
+    where
+      encodeHelper (1,x) = Single x
+      encodeHelper (n,x) = Multiple n x
