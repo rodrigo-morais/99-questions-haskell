6127f494caa78cec74300184c19d20be39f0d19a
Rodrigo Morais <morais.rm@gmail.com>
2016-06-06 08:36:27 -0300

    Adding the exercise 16.

--- 11to20.hs
+++ 11to20.hs
@@ -62,3 +62,8 @@ dupli xs = foldr (++) [] $ map (replicate 2) xs
 -- exercise 15
 repli :: [a] -> Int -> [a]
 repli xs n = foldr (++) [] $ map (replicate n) xs
+
+-- exercise 16
+dropEvery :: [a] -> Int -> [a]
+dropEvery [] _ = []
+dropEvery xs n = (take (n-1) xs) ++ dropEvery (drop n xs) n
